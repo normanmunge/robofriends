eb250c96f4e257878a0765a6ab1b290e61a4a8b7
Norman Munge <normanmunge@gmail.com>
2020-08-18 08:43:10 +0100

    Adds a hello component and playing around with react syntax

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello'
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <Hello greeting={'Hello React Ninja'}/>,
   document.getElementById('root')
 );
 
